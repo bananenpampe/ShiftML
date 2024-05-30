7ad8bfec1018e491b5d51cc830771d67e3dcbf3b
coffeedealer <ruben.rodriguezmadrid@epfl.chm>
2024-05-30 18:25:47 +0200

    adds an example notebook for chemical shieldings

--- install_rascaline.sh
+++ install_rascaline.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-conda install -c conda-forge cxx-compiler gcc rust python=3.10
+
+
+
+conda install -c conda-forge cxx-compiler 
+conda install -c conda-forge gcc 
+conda install -c conda-forge rust 
+conda install python=3.10
 
 export PATH="$CONDA_PREFIX/bin:$PATH"
 export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:$LD_LIBRARY_PATH"
 
-export CC="$CONDA_PREFIX/bin/gcc"
-export CXX="$CONDA_PREFIX/bin/g++"
+export CC="/usr/bin/gcc"
+export CXX="/usr/bin/g++"
 
-export CMAKE_C_COMPILER="$CONDA_PREFIX/bin/gcc"
-export CMAKE_CXX_COMPILER="$CONDA_PREFIX/bin/g++"
+export CMAKE_C_COMPILER="/usr/bin/gcc"
+§export CMAKE_CXX_COMPILER="/usr/bin/g++"
 
 #purge conda cache
-conda clean --all
+#conda clean --all
 
 #purge the pip cache
-pip cache purge
+#pip cache purge
 
 #install plumed and pyplumed from i-pi-demo channel with crystallization enabled
 
@@ -27,6 +33,7 @@ pip install metatensor
 pip install metatensor-core
 pip install metatensor-operations
 pip install metatensor-torch
+pip install scikit-learn
 
 pip install git+https://github.com/Luthaf/rascaline
 pip install --extra-index-url https://download.pytorch.org/whl/cpu git+https://github.com/luthaf/rascaline#subdirectory=python/rascaline-torch
